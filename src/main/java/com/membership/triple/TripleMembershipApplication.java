e2a4dc5cf734b6290f69c53b137c9093f54ef822
jbb9229 <jbb9229@gmail.com>
2022-06-22 14:40:31 +0900

    Initial commit

---
+++ src/main/java/com/membership/triple/TripleMembershipApplication.java
@@ -0,0 +1,13 @@
+package com.membership.triple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleMembershipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripleMembershipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/membership/triple/TripleMembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.membership.triple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleMembershipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
